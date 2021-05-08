a7e3ee7060072dfeaa49944665d74a762a5c7684
CC11001100 <cc11001100@qq.com>
2021-05-09 02:30:53 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# 知识星球自动点赞脚本
+
+
+## 一、安装
+脚本已上传到油猴官方商店，在商店安装脚本即可：  
+  
+[https://greasyfork.org/zh-CN/scripts/426175](https://greasyfork.org/zh-CN/scripts/426175)  
+
+![](markdown-images/README_images/985e0a50.png)
+
+
+## 二、使用 
+
+先将本油猴脚本置于禁用状态：  
+
+![](markdown-images/README_images/9dae43e2.png)  
+
+然后访问知识星球的官网：
+
+[https://wx.zsxq.com/](https://wx.zsxq.com/)
+
+登录后会跳转到主页，在左边选择要点赞的星球，单击进入（这里只是随便拿一个星球作为例子）：
+
+![](markdown-images/README_images/bfb5d0d0.png)
+
+注意进入之后地址栏上的链接会变为星球对应的地址：
+
+![](markdown-images/README_images/d4617e26.png)  
+
+然后在油猴的管理界面将本脚本打开：  
+
+![](markdown-images/README_images/25b49d85.png)
+
+刷新页面，开始自动刷赞了：
+![](markdown-images/README_images/GIF.gif)
+
+刷赞完成之后会提示本次点赞了多少条动态： 
+
+![](markdown-images/README_images/36f40651.png)
+
+然后把此脚本禁用，把页面即可。
+
+
---
+++ main.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name         知识星球自动点赞脚本
+// @namespace    CC11001100.com
+// @version      0.2
+// @description  知识星球自动化点赞，解放双手！
+// @author       CC11001100
+// @match       *://*/*
+// @grant        none
+// ==/UserScript==
+(async () => {
+
+    let bangCount = 0;
+    let c = 5;
+    while (true) {
+        try {
+            const bangElementList = document.querySelectorAll("div[title=点赞]");
+            for (let bangElement of bangElementList) {
+                if (bangElement.getAttribute("class") !== "like") {
+                    continue;
+                }
+                // https://developer.mozilla.org/zh-CN/docs/Web/API/Element/scrollIntoView
+                bangElement.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
+                await sleep(500);
+                bangElement.click();
+                await sleep(500)
+                bangCount++;
+            }
+
+            // 滚动到没有内容时的标记，为了防止网速慢到石化，退出时多给点机会
+            if (document.querySelector(".no-more") !== null) {
+                c--;
+                await sleep(1000);
+                if (c <= 0) {
+                    break;
+                }
+            }
+
+            await scrollToBottom();
+        } catch (e) {
+            alert("哦豁完蛋，报错了： " + e.message)
+        }
+        await sleep(1000);
+    }
+    alert(`点赞完成，本次共点赞${bangCount}条动态！`);
+
+    async function sleep(mils) {
+        return new Promise((resolve) => setTimeout(resolve, mils));
+    }
+
+    async function scrollToBottom() {
+        // https://developer.mozilla.org/zh-CN/docs/Web/API/Window/scrollTo
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: "smooth"
+        });
+        await sleep(100);
+        window.scrollTo({
+            top: document.body.scrollHeight - 300,
+            behavior: "smooth"
+        });
+        await sleep(100);
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: "smooth"
+        });
+    }
+
+})();
